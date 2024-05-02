fb6983edefbc36cddb1fe566c7b750e97147be2f
Ma161ric <Ma161ric@htwg-konstanz.de>
2024-05-02 22:52:18 +0200

    fix umlaut

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,29 @@
 export default function Page() {
 	return (
-		<div className="flex flex-col items-center justify-center mx-10">
+		<div className="mx-10 flex flex-col items-center justify-center">
 			<div className="text-center">
 				<h1 className="text-4xl font-medium">
 					Herzlich Willkommen zur Umfrage
 				</h1>
-				<p className="border-b-borderSeparatorColor m-3 border-b-2"></p>
+				<div className="m-3 border-b-2 border-b-borderSeparatorColor"></div>
 				<h3 className="font-semibold">So funkionierts:</h3>
 				<p>
-					Beantworten Sie bitte zunächst einige kurze Fragen zu Ihrer
-					Einkaufssituation. Wählen Sie im folgenden Schritt Ihre
-					Einkaufsreihenfolge aus und wie lange Sie an den jeweiligen
-					Bereichen verweilen. Sie werden an zwei kurzen Umfragen zu
-					unterschiedlichen Supermarktlayouts teilnehmen.
+					Beantworten Sie bitte zun&auml;chst einige kurze Fragen zu
+					Ihrer Einkaufssituation. W&auml;hlen Sie im folgenden
+					Schritt Ihre Einkaufsreihenfolge aus und wie lange Sie an
+					den jeweiligen Bereichen verweilen. Sie werden an zwei
+					kurzen Umfragen zu unterschiedlichen Supermarktlayouts
+					teilnehmen.
 				</p>
-				<h3 className="font-semibold my-6">
+				<h3 className="my-6 font-semibold">
 					Mit Ihrer Teilnahme helfen Sie uns, die Gestaltung von
-					Supermärkten zu verbessern!
+					Superm&auml;rkten zu verbessern!
 				</h3>
-				<h3 className="font-semibold my-6">
-					Vielen Dank für Ihre Unterstützung!
+				<h3 className="my-6 font-semibold">
+					Vielen Dank f&uuml;r Ihre Unterst&uuml;tzung!
 				</h3>
 			</div>
-			<button className="my-6 border-2 border-buttonBorderColor text-buttonBorderColor bg-buttonBackgroundColor p-7">
+			<button className="my-6 border-2 border-buttonBorderColor bg-buttonBackgroundColor p-7 text-buttonBorderColor">
 				Umfrage Starten
 			</button>
 		</div>
