46800b7bafe4833c0ad04d55356e1e54ae0b1c8d
Huan Nguyen <nguyenthieuhuan@gmail.com>
2024-05-20 12:49:34 +0200

    share layout in user pages

--- src/app/(user)/thankYou/layout.tsx
+++
@@ -1,5 +0,0 @@
-import DefaultLayout from "utils/layoutGenerator";
-import Header from "components/Header";
-import Footer from "components/Footer";
-
-export default DefaultLayout(() => Header(false), Footer);
