ea5b80c106c7b016b7945b1a5d516aabd52b4057
Huan Nguyen <nguyenthieuhuan@gmail.com>
2024-07-04 21:27:02 +0200

    delete useless directory

--- src/app/(user)/huan/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { FormProvider } from "../(survey)/FormContext";
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-	return <FormProvider>{children}</FormProvider>;
-}
