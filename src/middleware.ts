33e2a5cefef6dee41c2f2b216b135a38885367e7
Huan Nguyen <nguyenthieuhuan@gmail.com>
2024-07-05 18:40:49 +0200

    /login should redirect to admin when authenticated

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,28 +1,42 @@
 import { cookies } from "next/headers";
 import type { NextRequest } from "next/server";
 
-export async function middleware(request: NextRequest) {
-	console.log("my time!");
+function authenticate(request: NextRequest) {
 	const session = cookies().get("session");
 	if (session) {
 		const sessionKey = process.env.NEXT_PUBLIC_SESSION_SECRET;
 		if (sessionKey && sessionKey == session.value) {
-			console.log(
-				`authenticated key key: ${sessionKey}, session: ${session.value}`,
-			);
-			return;
+			console.log(request.url);
+			return true;
 		} else {
 			console.log(
 				`session did not equal key key: ${sessionKey}, session: ${session.value}`,
 			);
-			return Response.redirect(new URL("/login", request.url));
+			return false;
 		}
 	} else {
 		console.log("no session found");
-		return Response.redirect(new URL("/login", request.url));
+		return false;
+	}
+}
+
+export async function middleware(request: NextRequest) {
+	const authenticated = authenticate(request);
+	if (request.nextUrl.pathname.startsWith("/admin")) {
+		if (authenticated) {
+			return;
+		} else {
+			return Response.redirect(new URL("/login", request.url));
+		}
+	} else {
+		if (authenticated) {
+			return Response.redirect(new URL("/admin", request.url));
+		} else {
+			return;
+		}
 	}
 }
 
 export const config = {
-	matcher: "/admin/:path*",
+	matcher: ["/admin/:path*", "/login", "/admin"],
 };
