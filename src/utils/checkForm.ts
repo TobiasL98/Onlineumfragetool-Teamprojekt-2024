22fd25ecec1f9808da789113b483893c67a113aa
Huan Nguyen <nguyenthieuhuan@gmail.com>
2024-07-07 16:16:26 +0200

    Fix checkForm

--- src/utils/checkForm.ts
+++ src/utils/checkForm.ts
@@ -8,7 +8,7 @@ import {
 	sex,
 } from "app/(user)/(survey)/FormContext";
 
-export enum Error {
+export enum FormError {
 	time,
 	days,
 	allergies,
@@ -21,59 +21,63 @@ export enum Error {
 	sex,
 }
 
-export default function checkForm(form: typeof defaultFormState): Error[] {
-	let errors: Error[] = [];
+export default function checkForm(form: typeof defaultFormState): FormError[] {
+	let errors: FormError[] = [];
 
-	const error = (cond: boolean, type: Error) => {
+	const error = (cond: boolean, type: FormError) => {
 		if (cond) {
 			errors.push(type);
 		}
 	};
 
 	// does it have a valid time? does it have a time? empty string means it is not selected, it is not part of the time array so no need to check
-	error(!time.map((x) => x.value).includes(form.time), Error.time);
+	error(!time.map((x) => x.value).includes(form.time), FormError.time);
 
 	error(
 		days.every((x) => {
 			return !(form as any)[x.value];
 		}),
-		Error.days,
+		FormError.days,
 	);
 
 	error(
 		form.noPreferredDay &&
 			days.slice(0, -1).some((x) => (form as any)[x.value]),
-		Error.days,
+		FormError.days,
 	);
 
-	error(null == form.otherAllergies.match("(w* ?)*"), Error.allergies);
-	error(isNaN(form.age as any), Error.age);
+	error(null == form.otherAllergies.match("(w* ?)*"), FormError.allergies);
+	error(isNaN(form.age as any), FormError.age);
 	const adults = Number(form.adults);
-	error(isNaN(adults) || adults < 0, Error.adults);
+	error(isNaN(adults) || adults < 0, FormError.adults);
 	const children = Number(form.children);
-	error(isNaN(children) || children < 0, Error.children);
+	error(isNaN(children) || children < 0, FormError.children);
 
-	error(form.nobody && (adults > 0 || children > 0), Error.nobody);
+	error(form.nobody && (adults > 0 || children > 0), FormError.nobody);
 
 	error(
 		form.noAllergies &&
 			allergies.slice(0, -2).some((x) => (form as any)[x.value]),
-		Error.allergies,
-	);
-	error(form.noAllergies && form.otherAllergies.length > 0, Error.allergies);
-
-	error(
-		occupation.some((x) => form.occupation == x.value),
-		Error.occupation,
+		FormError.allergies,
 	);
 	error(
-		diet.some((x) => form.diet == x.value),
-		Error.diet,
+		form.noAllergies && form.otherAllergies.length > 0,
+		FormError.allergies,
 	);
 
 	error(
-		sex.some((x) => form.sex == x.value),
-		Error.sex,
+		form.occupation.length !== 0 &&
+			!occupation.some((x) => form.occupation == x.value),
+		FormError.occupation,
+	);
+	error(
+		form.diet.length !== 0 && !diet.some((x) => form.diet == x.value),
+		FormError.diet,
+	);
+
+	error(
+		form.sex.length !== 0 && !sex.some((x) => form.sex == x.value),
+		FormError.sex,
 	);
 
 	return errors;
