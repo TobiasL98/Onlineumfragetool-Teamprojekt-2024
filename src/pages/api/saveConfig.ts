f304b19e7dafa703773a1ce7a88b5352b0a4c5df
Huan Nguyen <nguyenthieuhuan@gmail.com>
2024-07-10 15:39:56 +0200

    Do not allow unauthorized access to change the layout

--- src/pages/api/saveConfig.ts
+++ src/pages/api/saveConfig.ts
@@ -1,27 +1,40 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import fs from 'fs';
-import path from 'path';
+import { NextApiRequest, NextApiResponse } from "next";
+import fs from "fs";
+import path from "path";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'POST') {
-        const { newConfig } = req.body;
+	if (
+		!req.cookies.session ||
+		req.cookies.session != process.env.NEXT_PUBLIC_SESSION_SECRET
+	) {
+		res.status(401).send("Not Allowed: Not Authenticated");
+		return;
+	}
+	if (req.method === "POST") {
+		const { newConfig } = req.body;
 
-        if (!newConfig) {
-            return res.status(400).json({ error: 'No config data provided' });
-        }
+		if (!newConfig) {
+			return res.status(400).json({ error: "No config data provided" });
+		}
 
-        try {
-            const filePath = path.join(process.cwd(), 'public', 'current_layout.json');
-           // to update the typical supermarket json, use the filePath below
-           // const filePath = path.join(process.cwd(), 'public', 'typical_supermarket.json');
-            fs.writeFileSync(filePath, JSON.stringify(newConfig, null, 2));
-            return res.status(200).json({ message: 'Config saved successfully' });
-        } catch (error) {
-            console.error('Error saving config:', error);
-            return res.status(500).json({ error: 'Failed to save config' });
-        }
-    } else {
-        res.setHeader('Allow', ['POST']);
-        res.status(405).end(`Method ${req.method} Not Allowed`);
-    }
+		try {
+			const filePath = path.join(
+				process.cwd(),
+				"public",
+				"current_layout.json",
+			);
+			// to update the typical supermarket json, use the filePath below
+			// const filePath = path.join(process.cwd(), 'public', 'typical_supermarket.json');
+			fs.writeFileSync(filePath, JSON.stringify(newConfig, null, 2));
+			return res
+				.status(200)
+				.json({ message: "Config saved successfully" });
+		} catch (error) {
+			console.error("Error saving config:", error);
+			return res.status(500).json({ error: "Failed to save config" });
+		}
+	} else {
+		res.setHeader("Allow", ["POST"]);
+		res.status(405).end(`Method ${req.method} Not Allowed`);
+	}
 }
